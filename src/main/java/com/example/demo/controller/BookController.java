f050e73415710758e6fa73ecd0009bf7883bbe20
tkaburagi1214 <tkaburagi@pivotal.io>
2017-12-25 00:31:14 +0900

    added persistence disk

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,11 @@ public class BookController {
 	@Autowired
 	private BookRepo repo;
 	
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+	
 	@RequestMapping("/books")
 	public List<Book> showAllBooks() {
+		logger.info("Called showAllBooks.");
 		return repo.findAll();
 	}
 
